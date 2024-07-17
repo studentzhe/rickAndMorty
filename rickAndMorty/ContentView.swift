2c04f3bf05813c6d6e4a354b4f623d516cbd4400
studentzhe <enzhegaysina@yandex.ru>
2024-07-17 17:11:59 +0500

    Initial Commit

---
+++ rickAndMorty/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  rickAndMorty
+//
+//  Created by test on 17.07.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+        ///
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ rickAndMorty/rickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  rickAndMortyApp.swift
+//  rickAndMorty
+//
+//  Created by test on 17.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct rickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
